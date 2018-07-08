58006a93358c9b3d9c8f1a3e048f177ef683e5d8
zouyingjie <yingjiezou1@gmail.com>
2018-07-08 14:58:03 +0800

    review

--- libs/openailab_faceapi/python/test.py
+++ libs/openailab_faceapi/python/test.py
@@ -1,55 +1,10 @@
 import openailabfaceapi
 
-# initialization
-# openailabfaceapi.initial("../log/")
-#
-# # check if there's a face in picture
-# jpegFileName="../data/3_1.jpg"
-# rect = openailabfaceapi.FaceExisted(jpegFileName)
-# print("left:", rect.left)
-# print("top:", rect.top)
-# print("width:", rect.width)
-# print("height:", rect.height)
-# if rect.width == 0 :
-#     print("face is not existed")
-# else :
-#     print("face is existed")
-#
-# # check if the quality is ok
-# threshold = 0.8
-# ret = openailabfaceapi.FaceQualityOK(jpegFileName, rect, threshold)
-# if ret == 0 :
-#     print("picture qualify is good")
-# else :
-#     print("picture is not good", ret)
-#
-# # check if the same person in lots of pictures
-# jpegFileNameList=["../data/3_1.jpg", "../data/3_2.jpg", "../data/3_3.jpg", "../data/3_4.jpg", "../data/3_5.jpg"]
-# threshold = 0.50
-# ret = openailabfaceapi.FaceIsSamePerson(jpegFileNameList, threshold)
-# if ret == 0 :
-#     print("pictures are not a person")
-# else :
-#     print("pictures are a person")
-#
-# # Select the best picture
-# threshold = 0.8
-# index = openailabfaceapi.BestFacePicture(jpegFileNameList, threshold)
-# print(jpegFileNameList[index] + " is the best picture")
-#
-# # deinitialization
-# openailabfaceapi.deinitial()
-
-
-from settings.settings import PROJECT_HOME
-
-# initialization
-# openailabfaceapi.initial("../log/")
-
-# check if there's a face in picture
 PROJECT_HOME = "/home/chiyuan/projects/face_service"
+
 def face_existed(image):
-    return openailabfaceapi.FaceExisted(jpegFileName)
+    return openailabfaceapi.FaceExisted(image)
+
 jpegFileName="{project_home}/libs/openailab_faceapi/data/3_1.jpg".format(project_home=PROJECT_HOME)
 # rect = openailabfaceapi.FaceExisted(jpegFileName)
 # print("left:", rect.left)
