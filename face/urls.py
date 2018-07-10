d83e0e8f58fda32abed053816da64d9b0a7eb07b
zouyingjie <yingjiezou1@gmail.com>
2018-07-10 19:21:01 +0800

    code review

--- face/urls.py
+++ face/urls.py
@@ -6,6 +6,7 @@ from face.views import FaceValidApiView
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^api/v2/face-valid/', FaceValidApiView.as_view(), name="api_v2_face_valid_view")
+urlpatterns = patterns(
+    '',
+    url(r'^api/v2/face-valid/$', FaceValidApiView.as_view(), name="api_v2_face_valid_view")
 )
