528b015966334799662ed6d1f308ecf0977e8391
zouyingjie <yingjiezou1@gmail.com>
2018-07-10 19:26:49 +0800

    code review

--- face/views.py
+++ face/views.py
@@ -2,6 +2,7 @@
 from orange.views import QingChengApiGetMixin
 
 from face.service import FaceIdService
+from openailab_faceapi.python import openailabfaceapi
 
 class FaceValidApiView(QingChengApiGetMixin):
 
@@ -16,7 +17,7 @@ class FaceValidApiView(QingChengApiGetMixin):
             "/home/chiyuan/projects/face_service/face/openailab_faceapi/data/3_4.jpg",
             "/home/chiyuan/projects/face_service/face/openailab_faceapi/data/3_5.jpg",
         ]
-        rect = self.get_model_service().face_image_rectangle(image=image)
+        rect = openailabfaceapi.FaceExisted(image=image)
         return {"width": rect.width}
         # face_valid, error_code = self.get_model_service().face_valid(new_image=new_image, old_images=old_images)
         # json_data = {"valid": face_valid, "code": error_code}
