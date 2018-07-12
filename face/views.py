772ff89df32b229254afa02ca455fc043cb7daa1
zouyingjie <yingjiezou1@gmail.com>
2018-07-12 17:16:44 +0800

    code review

--- face/views.py
+++ face/views.py
@@ -7,7 +7,7 @@ class FaceValidApiView(QingChengApiGetMixin):
 
     def get_request_params(self, request, *args, **kwargs):
         params = self.get_params(request, *args, **kwargs)
-        params["photo"] = "{project_home}/data/image/3_1.jpg".format(project_home=PROJECT_HOME)
+        # params["photo"] = "{project_home}/data/image/3_1.jpg".format(project_home=PROJECT_HOME)
         # photo = params.pop('photo', None)
         # old_photos = params.pop('old_photos', None)
         # service = FaceIdService()
@@ -19,8 +19,14 @@ class FaceValidApiView(QingChengApiGetMixin):
         #         path = service.down_photos(photo_url=photo)
         #         old_photo_paths.append(path)
         #     params['old_photos'] = old_photo_paths
+        return params
 
     def get_action_format(self, params, request, *args, **kwargs):
-        result = self.get_model_service().face_valid(**params)
+        # result = self.get_model_service().face_valid(**params)
+        # return result
+        service = FaceIdService()
+        image = "/home/chiyuan/openailab_faceapi/data/3_1.jpg"
+        rect = service.face_image_rectangle(image=image)
+        print(rect.width)
+        result = service.face_valid(image=image)
         return result
-
