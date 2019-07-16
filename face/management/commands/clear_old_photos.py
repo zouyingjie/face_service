f77ea09a52b916ef1257300aa430b32304e3fece
zouyingjie <yingjiezou1@gmail.com>
2019-07-16 16:25:06 +0800

    修改清理图片与校验日志脚本

--- face/management/commands/clear_old_photos.py
+++ face/management/commands/clear_old_photos.py
@@ -12,11 +12,15 @@ from settings.settings import PROJECT_HOME
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # 清理前一天的图片
+        # 清理前一天的图片和日志
         now = datetime_now()
         if now.hour != 1:
             return
         yesterday = datetime_delta(now, days=-1)
         path = "%s/data/image/%s*" % (PROJECT_HOME, date_to_str(yesterday.date()))
+        shell_command = "rm -rf %s" % path
+        os.system(shell_command)
+
+        log_path = "%s/data/logs/%s*" % (PROJECT_HOME, date_to_str(yesterday.date()).replace('-', ""))
         shell_command = "rm -rf %s" % path
         os.system(shell_command)
